6727469f0df8df1833e17bb8ef11fd2d68cdac7b
Juansu01 <thejuank0@hotmail.com>
2023-07-21 10:49:56 -0500

    Refactored CardContent

--- src/app/components/CardContent/CardContent.tsx
+++ src/app/components/CardContent/CardContent.tsx
@@ -1,4 +1,6 @@
 import styles from "./CardContent.module.css";
+import CardTitle from "./CardTitle";
+import CardDescription from "./CardDescription";
 import CardItemList from "./CardItemList";
 import CardItem from "./CardItem";
 import CardActionSection from "./CardActionSection";
@@ -6,12 +8,8 @@ import CardActionSection from "./CardActionSection";
 export default function CardContent() {
   return (
     <div className={styles["card-content"]}>
-      <h1 className={styles["card-title"]}>DiploAI</h1>
-      <p className={styles["card-description"]}>
-        DiploAI is a tool that helps companies and governments deal with
-        increasing climate complexity and regulatory change, by using GenAI to
-        identify their optimal strategy to achieve a given goal.
-      </p>
+      <CardTitle />
+      <CardDescription />
       <CardItemList>
         <CardItem />
         <CardItem />
