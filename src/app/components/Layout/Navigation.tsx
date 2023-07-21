d5b461aeb317331938d69278b7f844c7a3d31db1
Juansu01 <thejuank0@hotmail.com>
2023-07-21 13:07:22 -0500

    Made navigation responsive to smaller screens

--- src/app/components/Layout/Navigation.tsx
+++ src/app/components/Layout/Navigation.tsx
@@ -4,7 +4,7 @@ import styles from "./Navigation.module.css";
 export default function Navigation() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>Take Home Project</div>
+      <div className={`${styles.logo} text-sm`}>Take Home Project</div>
       <nav>
         <ul>
           <li>
