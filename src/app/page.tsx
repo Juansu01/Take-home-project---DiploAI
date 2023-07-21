39654e9c8c558f96ef4f447c43b891c0ad458000
Juansu01 <thejuank0@hotmail.com>
2023-07-21 13:41:00 -0500

    Importing form and updating FormContainer style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import { Fragment } from "react";
 
-import styles from "./page.module.css";
 import Navigation from "./components/Layout/Navigation";
 import FormContainer from "./components/UI/FormContainer";
+import Form from "./components/Form/Form";
 
 export default function Home() {
   return (
     <Fragment>
       <Navigation />
-      <main className={styles.main}>
-        <FormContainer />
+      <main className="d-flex justify-content-center pt-5">
+        <FormContainer>
+          <Form></Form>
+        </FormContainer>
       </main>
     </Fragment>
   );
