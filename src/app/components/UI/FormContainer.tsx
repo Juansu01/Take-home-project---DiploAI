770ede6499a345dcf7b2f03715ec7a27690ed85e
Juansu01 <thejuank0@hotmail.com>
2023-07-21 13:40:22 -0500

    Remove extra html

--- src/app/components/UI/FormContainer.tsx
+++ src/app/components/UI/FormContainer.tsx
@@ -2,9 +2,5 @@ import PropsChildren from "@/types/propsChildren";
 import styles from "./FormContainer.module.css";
 
 export default function FormContainer(props: PropsChildren) {
-  return (
-    <div className={styles["form-container"]}>
-      <h1 className="text-white">Fill this form</h1>
-    </div>
-  );
+  return <div className={styles["form-container"]}>{props.children}</div>;
 }
