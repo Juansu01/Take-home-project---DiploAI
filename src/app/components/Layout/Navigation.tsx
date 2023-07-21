d838bcf12f548ffde06ce06dbd3b057d461ef33d
Juansu01 <thejuank0@hotmail.com>
2023-07-21 11:21:45 -0500

    Add new navigation component

---
+++ src/app/components/Layout/Navigation.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import styles from "./Navigation.module.css";
+
+export default function Navigation() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>Take Home Project</div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/card">See Card</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
