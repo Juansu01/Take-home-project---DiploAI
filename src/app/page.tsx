323d737789058c347bc7ba30c334048995126a1d
Juansu01 <thejuank0@hotmail.com>
2023-07-22 05:58:54 -0500

    Added dummy todos and renamed TaskList to ToDoList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,30 @@ import { Fragment } from "react";
 import Navigation from "./components/Layout/Navigation";
 import FormContainer from "./components/UI/FormContainer";
 import Form from "./components/Form/Form";
-import TaskList from "./components/Task/TaskList";
+import ToDoList from "./components/ToDo/ToDoList";
+import { ToDoItem } from "@/types/todo";
 
 export default function Home() {
+  const DUMMY_TODOS: ToDoItem[] = [
+    {
+      id: 1,
+      userId: 1,
+      title: "TEST",
+      completed: true,
+    },
+    {
+      id: 2,
+      userId: 1,
+      title: "SECOND_TEST",
+      completed: false,
+    },
+    {
+      id: 3,
+      userId: 1,
+      title: "THIRD_TEST",
+      completed: true,
+    },
+  ];
   return (
     <Fragment>
       <Navigation />
@@ -16,7 +37,7 @@ export default function Home() {
       </section>
       <section className="d-flex justify-content-center pt-5 flex-row">
         <FormContainer>
-          <TaskList />
+          <ToDoList ToDoItems={DUMMY_TODOS} />
         </FormContainer>
       </section>
     </Fragment>
