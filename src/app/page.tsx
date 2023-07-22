10c8d5cc1ae23a30a002657e2d589a21b9770aa4
Juansu01 <thejuank0@hotmail.com>
2023-07-22 05:30:11 -0500

    Using TaskList in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,22 @@ import { Fragment } from "react";
 import Navigation from "./components/Layout/Navigation";
 import FormContainer from "./components/UI/FormContainer";
 import Form from "./components/Form/Form";
+import TaskList from "./components/Task/TaskList";
 
 export default function Home() {
   return (
     <Fragment>
       <Navigation />
-      <main className="d-flex justify-content-center pt-5">
+      <section className="d-flex justify-content-center pt-5 flex-row">
         <FormContainer>
           <Form></Form>
         </FormContainer>
-      </main>
+      </section>
+      <section className="d-flex justify-content-center pt-5 flex-row">
+        <FormContainer>
+          <TaskList />
+        </FormContainer>
+      </section>
     </Fragment>
   );
 }
