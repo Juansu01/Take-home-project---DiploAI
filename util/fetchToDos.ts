f90af0edd641a2a0384915b6ae598ccb8b961044
Juansu01 <thejuank0@hotmail.com>
2023-07-22 12:52:41 -0500

    Add Fisher-Yates (Knuth) Shuffle algorithm Fetching ToDos no longer retrieves repeaded ToDos

--- util/fetchToDos.ts
+++ util/fetchToDos.ts
@@ -1,10 +1,27 @@
 import { ToDoItem } from "@/types/todo";
 
+function generateUniqueNumbersArray(N: number): number[] {
+  // Create an array with numbers from 1 to N
+  const numbersArray: number[] = Array.from(
+    { length: N },
+    (_, index) => index + 1
+  );
+
+  // Fisher-Yates (Knuth) Shuffle algorithm
+  for (let i = numbersArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [numbersArray[i], numbersArray[j]] = [numbersArray[j], numbersArray[i]];
+  }
+
+  return numbersArray;
+}
+
 export async function fetchToDos(amount: number = 5): Promise<ToDoItem[]> {
   const ToDos: ToDoItem[] = [];
-  for (let i = 1; i <= amount; i++) {
+  const uniqueNumbersArray = generateUniqueNumbersArray(amount);
+  for (const item of uniqueNumbersArray) {
     const response = await fetch(
-      `https://jsonplaceholder.typicode.com/todos/${i}`
+      `https://jsonplaceholder.typicode.com/todos/${item}`
     );
     if (response.ok) {
       const toDo = await response.json();
