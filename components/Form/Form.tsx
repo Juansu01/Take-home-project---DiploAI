56611223433b20f1b87d24653a8a919751d5f7c0
Juansu01 <thejuank0@hotmail.com>
2023-07-22 12:13:38 -0500

    Add input change handler

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,16 +1,23 @@
 import styles from "./Form.module.css";
+import { FormProps } from "@/types/form";
+import React from "react";
 
-export default function Form() {
+export default function Form(props: FormProps) {
+  function inputChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    const userInput = +event.target.value;
+    props.setUserInput(userInput > 100 ? "100" : event.target.value);
+  }
   return (
-    <form>
+    <form onSubmit={props.onSubmit}>
       <div className={styles["form-controls"]}>
         <div className={styles["form-control"]}>
           <label className="h1">Amount of todos to fetch</label>
           <input
             type="number"
-            placeholder="Todo amount"
+            value={props.userInput}
             min="1"
             max="100"
+            onChange={inputChangeHandler}
           ></input>
         </div>
       </div>
