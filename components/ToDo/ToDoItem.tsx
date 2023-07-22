f9ae67277a78661545066c66c3dde394ba305d46
Juansu01 <thejuank0@hotmail.com>
2023-07-22 13:51:12 -0500

    Add colors to ToDoItem

--- components/ToDo/ToDoItem.tsx
+++ components/ToDo/ToDoItem.tsx
@@ -2,16 +2,20 @@ import styles from "./ToDoItem.module.css";
 import { ToDoItem } from "@/types/todo";
 
 export default function ToDoItem(props: ToDoItem) {
+  const completedColor = props.completed ? "text-success" : "text-warning";
+
   return (
-    <div className={`${styles["task-item"]} pt-4`}>
-      <div>
+    <div className={`${styles["task-item"]} pt-4 bg-dark`}>
+      <div className="text-primary">
         <h1>{props.title}</h1>
       </div>
       <div>
-        <h2>{props.id}</h2>
+        <h2 className="text-info">{props.id}</h2>
       </div>
       <div>
-        <h2>{props.completed ? "Completed" : "Incomplete"}</h2>
+        <h2 className={completedColor}>
+          {props.completed ? "Completed" : "Incomplete"}
+        </h2>
       </div>
     </div>
   );
